2bb7f285dcd76341656c2d6ff3287a567adb6a6b
darshangujraticodes <darshangujrati5@gmail.com>
2025-04-03 19:05:24 +0530

    nextjs

--- practice/app/(innerPages)/page.tsx
+++ practice/app/(innerPages)/page.tsx
@@ -1,11 +1,33 @@
 import ErrorButton from "../Components/ErrorButton";
+import Image from "next/image";
+// import img1 from "@/public/assets/1.png";
+
+import { assetsImg } from "@/public/assets/assets";
 
 export default function Home() {
-  return (
-    <div className="mt-8">
-      <h1 className="text-4xl text-center ">Home Page</h1>
+  const { img1, img2, img3, img4 } = assetsImg;
 
-      <ErrorButton />
-    </div>
+  return (
+    <section className="mt-8">
+      <div className="container">
+        <h1 className="text-4xl text-center ">Home Page</h1>
+
+        <ErrorButton />
+
+        <div className="mt-6 flex  gap-3">
+          <Image
+            alt="Thar dessert Tour"
+            src={img1}
+            className="w-[400px] h-[100%]"
+          />
+          <Image
+            alt="Varanasi Tour"
+            src={img2}
+            className="w-[400px] h-[100%]"
+          />
+          <Image alt="Goa Tour" src={img3} className="w-[400px] h-[100%]" />
+        </div>
+      </div>
+    </section>
   );
 }
